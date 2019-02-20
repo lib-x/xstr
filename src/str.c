16ea25837a5dec1307c1f1c3ff40b7baf61689aa
the-sushi <joshpritsker@protonmail.com>
2019-02-20 12:22:07 -0800

    fix xstr_insert and xstr_push... I'm an idiot

--- src/str.c
+++ src/str.c
@@ -52,6 +52,8 @@ x_error_t xstr_init(xstr_t * dest, size_t size)
  		return XE_ALLOC;
 	}
 
+	str->val[0] = '\0';
+
 	str->cap = size;
 	str->len = 0;
 
@@ -156,23 +158,16 @@ x_error_t xstr_cat(xstr_t dest, xstr_t src)
 	if (_dest->len > SIZE_MAX - _src->len)
 		return XE_OVERFLOW;
 
-	if (_dest->cap < _src->len + _dest->len) /* No need for addition end-of-string nil byte */
+	if (_dest->cap < _src->len + _dest->len)
 	{
-		if (_dest->len + _src->len < (SIZE_MAX / 2))
-		{
-			_dest->cap = (size_t) ((_src->len + _dest->len) * 2 - 1);
-		}
-		else
-		{
-			_dest->cap = (size_t) _src->len + _dest->len;
-		}
+		_dest->cap += _src->len;
 
 		_dest->val = realloc(_dest->val, _dest->cap + 1);
 		X_CHECK_ALLOC(_dest->val);
 	}
 
 	strcat(_dest->val, _src->val);
-	_dest->len = _dest->len + _src->len;
+	_dest->len += _src->len;
 
 	return XE_NONE;
 }
@@ -183,77 +178,44 @@ x_error_t xstr_cat_c_n(xstr_t dest, char * src, size_t src_len)
 
 	_dest = (struct _xstr_s *) dest;
 
-
 	if (_dest->len > SIZE_MAX - src_len)
 		return XE_OVERFLOW;
 
 	if (_dest->cap < src_len + _dest->len)
 	{
-		if (_dest->len + src_len < SIZE_MAX / 2)
-		{
-			_dest->cap = (size_t) ((src_len + _dest->len) * 2 - 1);
-		}
-		else 
-		{
-			_dest->cap = (size_t) src_len + _dest->len;
-		}
+		_dest->cap += src_len;
 
 		_dest->val = realloc(_dest->val, _dest->cap + 1);
 		X_CHECK_ALLOC(_dest->val);
 	}
 
-	strncat(_dest->val, src, src_len);
+	strcat(_dest->val, src);
 	_dest->len += src_len;
-	_dest->val[_dest->len + 1] = 0;
 
 	return XE_NONE;
 }
 
-/* POSSIBLE TODO: Write these better */
 x_error_t xstr_insert(xstr_t dest, xstr_t src, size_t index)
 {
 	char * tmp;
-	char * tmp1;
 	x_error_t err;
+	struct _xstr_s * _dest;
+	struct _xstr_s * _src;
 
-	if (src == NULL)
-	{
-		return XE_ISNULL;
-	}
+	_dest = (struct _xstr_s *) dest;
+	_src = (struct _xstr_s *) src;
 
-	tmp = strdup(*dest);
-	if (tmp == NULL)
-	{
-		return XE_DUP;
-	}
+	tmp = strdup(_dest->val + index);
+	if (tmp == NULL) return XE_DUP;
 
-	tmp1 = strdup(tmp + index);
-	if (tmp1 == NULL)
-	{
-		return XE_DUP;
-	}
+	_dest->val[index] = 0;
+	_dest->len = index;
 
-	tmp[index] = 0;
+	err = xstr_cat(dest, src);
+	if (err != XE_NONE) return err;
 
-	err = xstr_cpy_c(dest, tmp);
-	if (err != XE_NONE)
-	{
-		free(tmp);
-		free(tmp1);
-		return err;
-	}
-
-	err = xstr_cat_c_n(dest, *src, ((struct _xstr_s *) src)->len);
-	if (err != XE_NONE)
-	{
-		free(tmp);
-		free(tmp1);
-		return err;
-	}
-	err = xstr_cat_c(dest, tmp1);
-
+	err = xstr_cat_c(dest, tmp);
 	free(tmp);
-	free(tmp1);
 
 	return err;
 }
@@ -261,42 +223,22 @@ x_error_t xstr_insert(xstr_t dest, xstr_t src, size_t index)
 x_error_t xstr_insert_c_n(xstr_t dest, char * src, size_t index, size_t src_len)
 {
 	char * tmp;
-	char * tmp1;
 	x_error_t err;
+	struct _xstr_s * _dest;
 
-	tmp = strdup(*dest);
-	if (tmp == NULL)
-	{
-		return XE_DUP;
-	}
+	_dest = (struct _xstr_s *) dest;
 
-	tmp1 = strdup(tmp + index);
-	if (tmp == NULL)
-	{
-		return XE_DUP;
-	}
+	tmp = strdup(_dest->val + index);
+	if (tmp == NULL) return XE_DUP;
 
-	tmp[index] = 0;
-
-	err = xstr_cpy_c(dest, tmp);
-	if (err != XE_NONE)
-	{
-		free(tmp);
-		free(tmp1);
-		return err;
-	}
+	_dest->val[index] = 0;
+	_dest->len = index;
 
 	err = xstr_cat_c_n(dest, src, src_len);
-	if (err != XE_NONE)
-	{
-		free(tmp);
-		free(tmp1);
-		return err;
-	}
-	err = xstr_cat_c(dest, tmp1);
+	if (err != XE_NONE) return err;
 
+	err = xstr_cat_c(dest, tmp);
 	free(tmp);
-	free(tmp1);
 
 	return err;
 }
@@ -351,10 +293,10 @@ x_error_t xstr_push(xstr_t dest, char ch)
 
 	_dest = (struct _xstr_s *) dest;
 
-	if (_dest->cap > _dest->len + 1)
+	if (_dest->cap >= _dest->len + 1)
 	{
-		_dest->val[_dest->len + 1] = ch;
-		_dest->val[_dest->len + 2] = 0;
+		_dest->val[_dest->len] = ch;
+		_dest->val[_dest->len + 1] = 0;
 		_dest->len++;
 		return XE_NONE;
 	}
@@ -381,8 +323,8 @@ x_error_t xstr_push(xstr_t dest, char ch)
 
 	_dest->val = tmp;
 
-	_dest->val[_dest->len + 1] = ch;
-	_dest->val[_dest->len + 2] = 0;
+	_dest->val[_dest->len] = ch;
+	_dest->val[_dest->len + 1] = 0;
 	_dest->len++;
 
 	return XE_NONE;
